d3e38e7d286fbfcaaf3c160d8572f9be39434952
New <New@LAPTOP-9H29UN5F>
2020-05-20 16:51:13 +0530

    Leap year check method Finished

--- src/main/java/com/main/LeapYearFunctions.java
+++ src/main/java/com/main/LeapYearFunctions.java
@@ -3,8 +3,14 @@ package com.main;
 public class LeapYearFunctions {
 	
 	public String checkLeapYear(int year) {
-		return null;
+		if(year<1) {
+			return "Input should be Positive or greater then 0.";
+		}
 		
+		if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+	        return year+" is a Leap Year"; 
+	      else
+			 return year+" is not a Leap Year";
 	}
 
 }
