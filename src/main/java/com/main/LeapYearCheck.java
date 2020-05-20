63432c370d5148f63fa0cc79004ad5477f02b29e
New <New@LAPTOP-9H29UN5F>
2020-05-20 16:47:44 +0530

    Main method and test cases ready

--- src/main/java/com/main/LeapYearCheck.java
+++ src/main/java/com/main/LeapYearCheck.java
@@ -1,5 +1,18 @@
 package com.main;
 
+import java.util.Scanner;
+
 public class LeapYearCheck {
 
+	
+	public static void main(String[] args) {
+		int year;
+	    System.out.println("Enter an Year :: ");
+	    Scanner sc = new Scanner(System.in);
+	    year = sc.nextInt();
+	    System.out.println("Entered Year is "+year);
+	    LeapYearFunctions leapYearFunctions =new LeapYearFunctions();
+	    String Result=leapYearFunctions.checkLeapYear(year);
+	    System.out.println(Result);
+	}
 }
--- src/test/java/com/main/LeapYearFunctionsTest.java
+++ src/test/java/com/main/LeapYearFunctionsTest.java
@@ -2,13 +2,44 @@ package com.main;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class LeapYearFunctionsTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private LeapYearFunctions leapYearsFunctions;
+	
+	@BeforeEach
+	void initBefore() {
+//		fail("Not yet implemented");
+		leapYearsFunctions = new LeapYearFunctions();
+	}
+	
+	@Nested
+	@DisplayName("TestCheckLeapYearMethod")
+	class testcheckLeapYear {
+
+		@Test
+		@DisplayName("Testing Input is positive and greater then 0")
+		void testPositiveNumber() {
+			assertEquals("Input should be Positive or greater then 0.", leapYearsFunctions.checkLeapYear(-1234),"Input should be Positive Number.");
+		}
+		
+		@Test
+		@DisplayName("Testing Input Year is a Leap Year")
+		void testLeapYear() {
+			int year=2000;
+			assertEquals(year+" is a Leap Year", leapYearsFunctions.checkLeapYear(year));
+		}
+		
+		@Test
+		@DisplayName("Testing Input Year is Not a Leap Year")
+		void testNotLeapYear() {
+			int year=2000;
+			assertEquals(year+" is not a Leap Year", leapYearsFunctions.checkLeapYear(year));
+		}
 	}
 
 }
